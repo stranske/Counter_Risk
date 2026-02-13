49b459bfc2d284795296f6bc6997e647c9744b03
stranske <stranske@gmail.com>
2026-02-13 21:36:00 +0000

    feat: add pptx screenshot replacement writer and tests

---
+++ src/counter_risk/writers/__init__.py
@@ -0,0 +1,5 @@
+"""PPTX writer helpers for screenshot replacement workflows."""
+
+from counter_risk.writers.pptx_screenshots import replace_screenshot_pictures
+
+__all__ = ["replace_screenshot_pictures"]
